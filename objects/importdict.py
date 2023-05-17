1caf9ed3eed447496adcd108f4fbe0d7ee6a129a
George Pinchuk <gkpinchuk@hotmail.com>
2023-05-17 22:36:47 +1000

    Created a new dict type based around files.

--- objects/importdict.py
+++ objects/importdict.py
@@ -1,7 +1,12 @@
-from typing import TypedDict
+from typing import TypedDict, Tuple
 
 
 class ImportDict(TypedDict):
     import_name: str
     import_type: str
     full_path: str
+
+class FileDict(TypedDict):
+    file_name: str
+    imports: Tuple(str, str)
+    file_path: str
