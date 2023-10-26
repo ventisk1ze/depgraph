82d6e541674c9121f2c1629db0850b4e4a9c3909
George Pinchuk <gkpinchuk@hotmail.com>
2023-10-26 23:07:46 +1000

    Prettified JSON output and excluded envs from consideration

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from project import Project
 p = Project(r'C:\Users\User\Desktop\Programming\pm4py-core')
 
 with open('./test.json', 'w') as file:
-    json.dump(p.get_imports(), file)
+    json.dump(p.get_imports(), file, indent=4, ensure_ascii=False)
--- project.py
+++ project.py
@@ -9,7 +9,7 @@ class Project():
         for parent_path, _, filenames in os.walk(directory):
             for f in filenames:
                 all_files.append(os.path.join(parent_path, f))
-        self.file_paths = [x for x in all_files if x.endswith('.py')]
+        self.file_paths = [x for x in all_files if x.endswith('.py') and 'env' not in x]
         self.import_names = [self._get_file_name(x) for x in self.file_paths]
 
     @staticmethod
@@ -24,6 +24,7 @@ class Project():
                 if line.startswith('import ') or line.startswith('from ')
             ]
             imports = [x.split('.')[-1] if '.' in x else x for x in imports]
+        
         file: FileDict = {
             'file_name': self._get_file_name(file_path),
             'imports': [
@@ -35,6 +36,7 @@ class Project():
                 ],
             'file_path': file_path
             }
+        
         return file
     
     def get_imports(self) -> dict:
