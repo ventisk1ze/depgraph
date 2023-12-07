4bf0cf7354e90740f61a16e3530f7afb612e647c
George Pinchuk <gkpinchuk@hotmail.com>
2023-12-07 22:44:04 +1000

    Baseline work with command line args added

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
-import json
+import math
+from datetime import datetime
 from project import Project
 from graph import Graph
 import matplotlib.pyplot as plt
 import networkx as nx
+import argparse
 
-p = Project(r'C:\Users\User\Desktop\Programming\rosreestr2coord\rosreestr2coord', debug=False)
+parser = argparse.ArgumentParser(
+    prog='Depgraph v0.0.1',
+    description='This module creates graph of project dependencies',
+    epilog='That`s all, folks!'
+)
+
+parser.add_argument('project', help='Path to project')
+parser.add_argument('-d', '--debug', action='store_true')
+parser.add_argument('-l', '--logging', action='store_true')
+
+args = parser.parse_args()
+
+start = datetime.now()
+print(f'Started processing at {start}')
+
+p = Project(args.project, debug=args.debug)
+print(f'Collected imports in {datetime.now() - start}')
 
 g = Graph(p.get_imports()).create_graph()
+print(f'Created nx graph in {datetime.now() - start}')
 
+color_map = ['red' if node in p.import_names else 'blue' for node in g.nodes().keys()]
 
-color_map = ['red' if node in p.import_names else 'blue' for node in list(g.nodes().keys())]
-
-
-nx.draw_kamada_kawai(g, with_labels=True, node_size=1000, node_color=color_map)
-
-with open('imports.txt', 'w+', encoding='utf-8') as f:
-    print(*p.import_names, sep='\n', file=f)
+nx.draw(g, pos=nx.spring_layout(g, k=10/math.sqrt(g.order())), with_labels=True, node_size=1000, node_color=color_map)
+print(f'Drew the graph in {datetime.now() - start}')
 
 plt.show()
